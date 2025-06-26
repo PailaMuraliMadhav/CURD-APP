5e0bc4869ba57c446ab0e5279151e8796b00bea3
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-26 13:03:02 +0530

    created todo

---
+++ src/config/dbconnect.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async()=>{
+    try{
+ await mongoose.connect("...");
+//  async is used to make sure that the code inside the async function is executed asynchronously.
+// asynchronus is the process of executing multiple tasks in parallel, without waiting for each task to complete before moving on to 
+//await is used to wait for the promise to resolve before continuing with the next line of code.
+
+console.log("✅Connected to MongoDB");}
+catch(err){
+    console.log("❌Mongo DB connection failed",error.message);
+    process.exit(1);// it terminates the code.
+}
+}
+
+
+
+
---
+++ src/routes/todo.routes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { createTodo, deleteTodo, getoneTodo, getTodoById, updateTodo } from '../controllers/todo.constroller';
+
+const router = express.Router();
+
+router.post('/',createTodo);// used to extract data from the user and pass it to the controller function
+router.get('/',getTodo); // to read the data from database
+
+router.get('/:id',getTodoById); // to read the data by user
+
+
+router.put('/:id',updateTodo);//to update the data by user
+
+router.delete('/:id',deleteTodo);//to delete the data by user
+
+export default router;
+
+
+
+
